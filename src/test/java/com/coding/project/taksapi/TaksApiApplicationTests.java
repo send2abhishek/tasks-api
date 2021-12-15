1881ffcafd7f412eaed97ff7620dfea52b282734
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-15 19:53:23 +0530

    feat: initial project setup

---
+++ src/test/java/com/coding/project/taksapi/TaksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.project.taksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
